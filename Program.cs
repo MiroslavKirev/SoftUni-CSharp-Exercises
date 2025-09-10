005c9f166848058ffd52d78d6e0765589b32fc26
Мирослав <123307505+MiroslavKirev@users.noreply.github.com>
2025-09-10 21:13:32 +0300

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int sum = a + b;
+
+        Console.WriteLine(sum);
+    }
+}
